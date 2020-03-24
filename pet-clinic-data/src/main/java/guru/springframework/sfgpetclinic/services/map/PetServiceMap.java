aee56f3a7da873dac6dddf9ae5ae679a2ed2ae78
darkonion <pawkapl89@gmail.com>
2020-03-24 14:14:58 +0100

    Implementing map based services. Closes #4.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/PetServiceMap.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/PetServiceMap.java
@@ -7,6 +7,7 @@ import java.util.Set;
 
 public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
 
+
     @Override
     public Set<Pet> findAll() {
         return super.findAll();
