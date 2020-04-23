a59ffb9a96904cbb78c0a3c9a7a178639c9dd03a
darkonion <pawkapl89@gmail.com>
2020-04-23 23:19:45 +0200

    refactored to project lombok Closes #51

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Speciality.java
@@ -1,19 +1,22 @@
 package guru.springframework.sfgpetclinic.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "specialities")
 public class Speciality extends BaseEntity {
 
     private String description;
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -75,6 +75,8 @@ public class DataLoader implements CommandLineRunner {
         owner1.setCity("Miami");
         owner1.setTelephone("12312");
 
+        Owner.builder().address("ASDF").build();
+
         Pet mikesPet = new Pet();
         mikesPet.setPetType(savedDogPetType);
         mikesPet.setOwner(owner1);
